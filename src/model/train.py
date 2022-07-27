1ed21c528b15db81e4359db80bf33d6a131cdebb
carlosasantos <37413649+carlosasantos@users.noreply.github.com>
2022-07-27 18:35:47 +0100

    fix linting problems

--- src/model/train.py
+++ src/model/train.py
@@ -40,7 +40,10 @@ def process_data(df):
                'Age']].values, df['Diabetic'].values
 
     # train/test split
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=0)
+    X_train, X_test, y_train, y_test = train_test_split(X,
+                                                        y,
+                                                        test_size=0.30,
+                                                        random_state=0)
 
     # return splits and encoder
     return X_train, X_test, y_train, y_test
@@ -53,6 +56,7 @@ def train_model(reg_rate, X_train, X_test, y_train, y_test):
 
     # return model
     return model
+
 
 def parse_args():
     # setup arg parser
@@ -69,6 +73,7 @@ def parse_args():
 
     # return args
     return args
+
 
 # run script
 if __name__ == "__main__":
